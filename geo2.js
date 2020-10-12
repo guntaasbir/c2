3806e7398b2aefceb193e8a7fc8a8196791b688c
guntaasbir <72479512+guntaasbir@users.noreply.github.com>
2020-10-12 11:52:58 -0600

    Add files via upload

---
+++ geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "EDMONTON",
+      'continent': "NA",
+      'country': "CA",
+      'region': "AB"
+    },
+    'ip':"108.173.147.220"
+  }]);
+})
+//
+()
+
+;
